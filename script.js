10426eb03efb091b8ee0a0bc75c20064c8135221
Banana2048 <146014034+Banana2048@users.noreply.github.com>
2024-09-11 15:13:37 -0700

    added ability to add more question & answer inputs
    - created a button to add a new set of question/answer inputs
    - renamed and reorganized question and answer divs
    - fixed extra answer </div>'s

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("test");
+//console.log("test");
+
+function handleNewQuestion()
+{
+    const newQuestionElement = document.querySelector('.js-questions-and-answers');
+
+    const newQuestionHTML = `
+        <div class = 'input-div js-input-div'>
+            <div class = 'question-entry-div'>
+                <input class = 'question-textbox' type = "text" placeholder = "Enter question"/>
+            </div>
+            
+            <div class = 'answer-entry-div'>
+                <input class = 'answer-textbox' type = "text" placeholder = "Enter answer"/>
+            </div>
+
+            <div class = 'answer-entry-div'>
+                <input class = 'answer-textbox' type = "text" placeholder = "Enter answer"/>
+            </div>
+        </div>
+    `;
+
+    newQuestionElement.innerHTML += newQuestionHTML;
+}
