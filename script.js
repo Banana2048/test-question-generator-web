9680e293f0ab59418b215fcfd4b437bf9de6abef
Banana2048 <146014034+Banana2048@users.noreply.github.com>
2024-09-11 18:27:07 -0700

    stores question & answer input into an array
    - fixed each question to have 4 answer choices
    - question & answer inputs are stored into an array when 'done' button is clicked

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//console.log("test");
+let test = [];
 
 let questionNum = 2;
 
@@ -9,22 +9,59 @@ function handleNewQuestion()
     const newQuestionHTML = `
         <div class = 'input-div js-input-div'>
             <div class = 'question-heading-div'>
-                    Question ${questionNum}
+                    question ${questionNum}
                 </div>
             <div class = 'question-entry-div'>
-                <input class = 'question-textbox' type = "text" placeholder = "Enter question"/>
+                <input class = 'question-textbox js-question-textbox' type = "text" placeholder = "Enter question"/>
             </div>
             
             <div class = 'answer-entry-div'>
-                <input class = 'answer-textbox' type = "text" placeholder = "Enter answer"/>
+                <input class = 'answer-textbox js-answer-textbox' type = "text" placeholder = "Enter answer"/>
             </div>
 
             <div class = 'answer-entry-div'>
-                <input class = 'answer-textbox' type = "text" placeholder = "Enter answer"/>
+                <input class = 'answer-textbox js-answer-textbox' type = "text" placeholder = "Enter answer"/>
+            </div>
+
+            <div class = 'answer-entry-div'>
+                <input class = 'answer-textbox js-answer-textbox' type = "text" placeholder = "Enter answer"/>
+            </div>
+
+            <div class = 'answer-entry-div'>
+                <input class = 'answer-textbox js-answer-textbox' type = "text" placeholder = "Enter answer"/>
             </div>
         </div>
     `;
 
     questionNum += 1;
     newQuestionElement.innerHTML += newQuestionHTML;
+}
+
+function doneButtonClick()
+{
+    const allQuestions = document.querySelectorAll('.js-question-textbox');
+    const allAnswers = document.querySelectorAll('.js-answer-textbox');
+    let curAnswerIndex = 0;
+
+    // add questions
+    for (let i = 0; i < allQuestions.length; i++)
+    {
+        let newQuestion = {
+            'question': "",
+            'answers': []};
+        
+        newQuestion['question'] = allQuestions[i].value;
+        
+        // add answer choices for each question
+        let tempAnswerIndex = curAnswerIndex;
+        for (let j = curAnswerIndex; j < tempAnswerIndex + 4 && curAnswerIndex < allAnswers.length; j++)
+        {
+            newQuestion['answers'].push(allAnswers[j].value);
+            curAnswerIndex++;
+        }
+
+        test.push(newQuestion);
+    }
+
+    console.log(test);
 }
