9cdd37e921ea8c7ef991d4aee09ee468751fd9ef
Banana2048 <146014034+Banana2048@users.noreply.github.com>
2024-09-11 17:24:08 -0700

    added a header for each question & answer section

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 //console.log("test");
 
+let questionNum = 2;
+
 function handleNewQuestion()
 {
     const newQuestionElement = document.querySelector('.js-questions-and-answers');
 
     const newQuestionHTML = `
         <div class = 'input-div js-input-div'>
+            <div class = 'question-heading-div'>
+                    Question ${questionNum}
+                </div>
             <div class = 'question-entry-div'>
                 <input class = 'question-textbox' type = "text" placeholder = "Enter question"/>
             </div>
@@ -20,5 +25,6 @@ function handleNewQuestion()
         </div>
     `;
 
+    questionNum += 1;
     newQuestionElement.innerHTML += newQuestionHTML;
 }
